1dfa14e9d0823ad8625e3682834bcf5481467927
ttakuma17 <ttanimiya@gmail.com>
2023-01-15 11:15:06 +0900

    timestamp の値を定数へ格納

--- src/main/java/com/rtjavamemoapp/application/controller/MemoControllerAdvice.java
+++ src/main/java/com/rtjavamemoapp/application/controller/MemoControllerAdvice.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,10 +20,13 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class MemoControllerAdvice {
 
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd - HH:mm:ss Z");
+    private final String EXCEPTION_OCCURRED_TIME = ZonedDateTime.now().format(formatter).toString();
+
     @ExceptionHandler(value = ResourceNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleNoResourceFound(ResourceNotFoundException e, HttpServletRequest request) {
         Map<String, String> body = Map.of(
-                "timestamp", ZonedDateTime.now().toString(),
+                "timestamp", EXCEPTION_OCCURRED_TIME,
                 "status", String.valueOf(HttpStatus.NOT_FOUND.value()),
                 "error", HttpStatus.NOT_FOUND.getReasonPhrase(),
                 "message", e.getMessage(),
