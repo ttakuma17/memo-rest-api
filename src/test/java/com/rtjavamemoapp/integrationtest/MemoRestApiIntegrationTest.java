67afd772bee380db24e897fc487c2b42dea4dd55
ttakuma17 <ttanimiya@gmail.com>
2023-01-30 23:06:43 +0900

    重複アノテーションの削除対応

--- src/test/java/com/rtjavamemoapp/integrationtest/MemoRestApiIntegrationTest.java
+++ src/test/java/com/rtjavamemoapp/integrationtest/MemoRestApiIntegrationTest.java
@@ -18,66 +18,60 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql(
+    scripts = {"classpath:/sqlannotation/delete-memos.sql",
+        "classpath:/sqlannotation/insert-memos.sql"},
+    executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
+)
+@Transactional
 public class MemoRestApiIntegrationTest {
 
   @Autowired
   MockMvc mockMvc;
 
   @Test
-  @Sql(
-      scripts = {"classpath:/sqlannotation/delete-memos.sql",
-          "classpath:/sqlannotation/insert-memos.sql"},
-      executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
-  )
-  @Transactional
-  void メモを全件取得できること() throws Exception{
-    String response = mockMvc.perform(MockMvcRequestBuilders.get("/memos")).
-        andExpect(MockMvcResultMatchers.status().isOk())
+  void メモを全件取得できること() throws Exception {
+    String response = mockMvc.perform(MockMvcRequestBuilders.get("/memos"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
         .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
 
     JSONAssert.assertEquals("[" +
-            " {" +
-            " \"id\": 1," +
-            " \"title\": \"第1回課題\"," +
-            " \"category\": \"Java\"," +
-            " \"description\": \"Hello World\"," +
-            " \"date\": \"2022/12/31\"," +
-            " \"markDiv\": 1" +
-            " }," +
-            " {" +
-            " \"id\": 2," +
-            " \"title\": \"第2回課題\"," +
-            " \"category\": \"Java\"," +
-            " \"description\": \"オリジナルクラスの実装\"," +
-            " \"date\": \"2023/01/01\"," +
-            " \"markDiv\": 1" +
-            " }," +
-            " {" +
-            " \"id\": 3," +
-            " \"title\": \"第3回課題\"," +
-            " \"category\": \"Java\"," +
-            " \"description\": \"ListとMapの練習\"," +
-            " \"date\": \"2023/01/02\"," +
-            " \"markDiv\": 1" +
-            " }," +
-            " {" +
-            " \"id\": 4," +
-            " \"title\": \"第4回課題\"," +
-            " \"category\": \"Java\"," +
-            " \"description\": \"Streamをお試し\"," +
-            " \"date\": \"2023/01/09\"," +
-            " \"markDiv\": 0" +
-            " }" +
-            "]", response, JSONCompareMode.STRICT);
+        " {" +
+        " \"id\": 1," +
+        " \"title\": \"第1回課題\"," +
+        " \"category\": \"Java\"," +
+        " \"description\": \"Hello World\"," +
+        " \"date\": \"2022/12/31\"," +
+        " \"markDiv\": 1" +
+        " }," +
+        " {" +
+        " \"id\": 2," +
+        " \"title\": \"第2回課題\"," +
+        " \"category\": \"Java\"," +
+        " \"description\": \"オリジナルクラスの実装\"," +
+        " \"date\": \"2023/01/01\"," +
+        " \"markDiv\": 1" +
+        " }," +
+        " {" +
+        " \"id\": 3," +
+        " \"title\": \"第3回課題\"," +
+        " \"category\": \"Java\"," +
+        " \"description\": \"ListとMapの練習\"," +
+        " \"date\": \"2023/01/02\"," +
+        " \"markDiv\": 1" +
+        " }," +
+        " {" +
+        " \"id\": 4," +
+        " \"title\": \"第4回課題\"," +
+        " \"category\": \"Java\"," +
+        " \"description\": \"Streamをお試し\"," +
+        " \"date\": \"2023/01/09\"," +
+        " \"markDiv\": 0" +
+        " }" +
+        "]", response, JSONCompareMode.STRICT);
   }
 
   @Test
-  @Sql(
-      scripts = {"classpath:/sqlannotation/delete-memos.sql",
-          "classpath:/sqlannotation/insert-memos.sql"},
-      executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
-  )
-  @Transactional
   void 指定したIDに紐づくメモを取得できること() throws Exception{
     String response = mockMvc.perform(MockMvcRequestBuilders.get("/memos/1")).
         andExpect(MockMvcResultMatchers.status().isOk())
@@ -95,24 +89,12 @@ public class MemoRestApiIntegrationTest {
   }
 
   @Test
-  @Sql(
-      scripts = {"classpath:/sqlannotation/delete-memos.sql",
-          "classpath:/sqlannotation/insert-memos.sql"},
-      executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
-  )
-  @Transactional
   void 存在しないID指定してメモを取得するとNotFoundを返すこと() throws Exception{
     mockMvc.perform(MockMvcRequestBuilders.get("/memos/100"))
         .andExpect(MockMvcResultMatchers.status().isNotFound());
   }
 
   @Test
-  @Sql(
-      scripts = {"classpath:/sqlannotation/delete-memos.sql",
-          "classpath:/sqlannotation/insert-memos.sql"},
-      executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
-  )
-  @Transactional
   void メモを新規作成できること() throws Exception {
     mockMvc.perform(MockMvcRequestBuilders.post("/memos")
         .contentType(MediaType.APPLICATION_JSON)
@@ -129,12 +111,6 @@ public class MemoRestApiIntegrationTest {
   }
 
   @Test
-  @Sql(
-      scripts = {"classpath:/sqlannotation/delete-memos.sql",
-          "classpath:/sqlannotation/insert-memos.sql"},
-      executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
-  )
-  @Transactional
   void メモを更新できること() throws Exception{
     mockMvc.perform(MockMvcRequestBuilders.patch("/memos/1")
         .contentType(MediaType.APPLICATION_JSON)
@@ -150,12 +126,6 @@ public class MemoRestApiIntegrationTest {
   }
 
   @Test
-  @Sql(
-      scripts = {"classpath:/sqlannotation/delete-memos.sql",
-          "classpath:/sqlannotation/insert-memos.sql"},
-      executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
-  )
-  @Transactional
   void 存在しないIDを指定してメモを更新するとNotFoundを返すこと() throws Exception{
     mockMvc.perform(MockMvcRequestBuilders.patch("/memos/100")
         .contentType(MediaType.APPLICATION_JSON)
@@ -171,24 +141,12 @@ public class MemoRestApiIntegrationTest {
   }
 
   @Test
-  @Sql(
-      scripts = {"classpath:/sqlannotation/delete-memos.sql",
-          "classpath:/sqlannotation/insert-memos.sql"},
-      executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
-  )
-  @Transactional
   void メモを削除できること() throws Exception {
     mockMvc.perform(MockMvcRequestBuilders.delete("/memos/1"))
         .andExpect(MockMvcResultMatchers.status().isOk());
   }
 
   @Test
-  @Sql(
-      scripts = {"classpath:/sqlannotation/delete-memos.sql",
-          "classpath:/sqlannotation/insert-memos.sql"},
-      executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
-  )
-  @Transactional
   void 存在しないIDを指定して削除を実行するとNotFoundが発生すること() throws Exception {
     mockMvc.perform(MockMvcRequestBuilders.delete("/memos/100"))
         .andExpect(MockMvcResultMatchers.status().isNotFound());
